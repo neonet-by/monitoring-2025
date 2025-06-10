03a9a64a0ef13209042ac3eb126e23f3455adf06
perhewz <149831064+Perhewz-Hellcat@users.noreply.github.com>
2025-06-11 00:27:09 +0300

    Create test_DB, create logout

---
+++ src/users.php
@@ -0,0 +1,11 @@
+<?php
+$users = [
+    'admin' => [
+        'password' => 'admin123',
+        'role' => 'admin'
+    ],
+    'user' => [
+        'password' => 'user123',
+        'role' => 'user'
+    ]
+];
