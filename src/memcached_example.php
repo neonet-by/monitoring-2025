bd928fe5b79bed6e2f0651f934581c39ca2cf6fe
root <root@nVesta>
2025-06-16 19:01:05 +0300

    Добавлены ещё примеры

--- src/memcached_example.php
+++ src/memcached_example.php
@@ -7,8 +7,14 @@ memcache_set($memcache,'test','456',0,10000);
 memcache_set($memcache,'test.540','Данные',0,10000);
 
 
-echo memcache_get($memcache,'test');
-echo '<br>';
 echo memcache_get($memcache,'test.540');
+echo '<br>';
+echo memcache_get($memcache,'test');
+
+
+memcache_set($memcache,'channel.a002.1.sc_error',0,0,10000);
+memcache_set($memcache,'channel.a002.1.pes_error',1212,0,10000);
+memcache_set($memcache,'channel.a004.2.sc_error',122,0,10000);
+memcache_set($memcache,'channel.a004.2.pes_error',711,0,10000);
 
 ?>
