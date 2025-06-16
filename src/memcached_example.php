8b229a5a8f1433ae9e6b2e4c34029b914e747518
ViktorKhvedorets <128993471+ViktorKhvedorets@users.noreply.github.com>
2025-06-16 15:48:27 +0300

    test

--- src/memcached_example.php
+++ src/memcached_example.php
@@ -1,14 +1,14 @@
 <?php
 
-$memcache = memcache_connect('localhost',11211);
+$memcache = new Memcache(); 
+$memcache->connect('localhost', 11211); 
 
-memcache_set($memcache,'test','123',0,10000);
-memcache_set($memcache,'test','456',0,10000);
-memcache_set($memcache,'test.540','Данные',0,10000);
+$memcache->set('test', '123', 0, 10000);
+$memcache->set('test', '456', 0, 10000);
+$memcache->set('test.540', 'Данные', 0, 10000);
 
-
-echo memcache_get($memcache,'test');
+echo $memcache->get('test');
 echo '<br>';
-echo memcache_get($memcache,'test.540');
+echo $memcache->get('test.540');
 
 ?>
